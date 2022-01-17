d0cd8bc941b5197b468938164f26afffdb1105dc
Anirudh Ramia <anirudh.ramia@nutanix.com>
2022-01-17 12:29:30 +1100

    created basic app to test slack calls

---
+++ Slack/app.py
@@ -0,0 +1,87 @@
+import os
+# Use the package we installed
+from slack_bolt import App
+from slack_sdk.errors import SlackApiError
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+channel_id = "U02TMFUK2CW"
+
+try:
+    # Call the chat.postMessage method using the WebClient
+    result = app.client.chat_postMessage(
+        channel=channel_id, 
+        text="Hello world"
+    )
+    app.logger.info(result)
+
+except SlackApiError as e:
+    app.logger.error(f"Error posting message: {e}")
+
+# Add functionality here
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+    
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+  
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
+
+
